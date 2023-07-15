dcaf0164a1b91114394e261606beac24f35d0073
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-15 14:27:38 +0900

    feat: create temporal NxCordic.cos_sin

--- lib/nx_cordic.ex
+++ lib/nx_cordic.ex
@@ -2,4 +2,10 @@ defmodule NxCordic do
   @moduledoc File.read!("README.md")
              |> String.split("<!-- MODULEDOC -->")
              |> Enum.fetch!(1)
+
+  import Nx.Defn
+
+  defn cos_sin(angles) do
+    {Nx.cos(angles), Nx.sin(angles)}
+  end
 end
