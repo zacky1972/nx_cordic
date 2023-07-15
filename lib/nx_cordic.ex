806bdfd511b0b409b6956567ad8124a0f3a12cd7
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-15 12:04:09 +0900

    initial commit

---
+++ lib/nx_cordic.ex
@@ -0,0 +1,18 @@
+defmodule NxCordic do
+  @moduledoc """
+  Documentation for `NxCordic`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NxCordic.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nx_cordic_test.exs
@@ -0,0 +1,8 @@
+defmodule NxCordicTest do
+  use ExUnit.Case
+  doctest NxCordic
+
+  test "greets the world" do
+    assert NxCordic.hello() == :world
+  end
+end
