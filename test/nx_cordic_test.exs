61c504a6feb9bb9116f0fca05c5c4c6afe3a24a6
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-15 14:28:15 +0900

    test: create test of cos_sin

--- test/nx_cordic_test.exs
+++ test/nx_cordic_test.exs
@@ -1,4 +1,10 @@
 defmodule NxCordicTest do
   use ExUnit.Case
   doctest NxCordic
+
+  test "cos_sin" do
+    input = NxCordic.Util.gen_input(256)
+
+    assert NxCordic.Util.equals_tuple_with_epsilon(NxCordic.cos_sin(input), NxCordic.Util.cos_sin(input), 0.0001) |> Nx.to_number() == 1
+  end
 end
