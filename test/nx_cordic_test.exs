9aab7eb34544e7a96410c328de1e5be6d277e0cf
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-16 04:39:52 +0900

    fix: apply format (#5)

--- test/nx_cordic_test.exs
+++ test/nx_cordic_test.exs
@@ -5,6 +5,11 @@ defmodule NxCordicTest do
   test "cos_sin" do
     input = NxCordic.Util.gen_input(256)
 
-    assert NxCordic.Util.equals_tuple_with_epsilon(NxCordic.cos_sin(input), NxCordic.Util.cos_sin(input), 0.0001) |> Nx.to_number() == 1
+    assert NxCordic.Util.equals_tuple_with_epsilon(
+             NxCordic.cos_sin(input),
+             NxCordic.Util.cos_sin(input),
+             0.0001
+           )
+           |> Nx.to_number() == 1
   end
 end
